28d60b7008119cb9fc31a0d285684ff563853f72
Phumiphat Wongsinghakul <136429943+jamesspw@users.noreply.github.com>
2023-09-20 11:24:31 +0700

    Update Merge.py

--- Merge.py
+++ Merge.py
@@ -1,30 +1,59 @@
-def merges(arr):
-  if len(arr) > 1:
-    l_arr = arr[:len(arr)//2]
-    r_arr = arr[len(arr)//2:]
-    merges(l_arr)
-    merges(r_arr)
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
 
-    x = y = z = 0
-    while x < len(l_arr) and y < len(r_arr):
-      if l_arr[x] < r_arr[y]:
-        arr[z] = l_arr[x]
-        x += 1
-      else:
-        arr[z] = r_arr[y]
-        y += 1
-      z += 1
-    while x < len(l_arr):
-      arr[z] = l_arr[x]
-      x += 1
-      z += 1
-    while y < len(r_arr):
-      arr[z] = r_arr[y]
-      y += 1
-      z += 1
+    # Divide the array into two halves
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    # Recursively sort both halves
+    left_half = merge_sort(left_half)
+    right_half = merge_sort(right_half)
+
+    # Merge the sorted halves
+    return merge(left_half, right_half)
 
 
-Before = [3, 5, 8, 6, 7, 4, 1 ,2]
-print('Before Merge :', Before)
-merges(Before)
-print('After Merge :', Before)
+def merge(left, right):
+    merged = []
+    left_index, right_index = 0, 0
+
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            merged.append(left[left_index])
+            left_index += 1
+        else:
+            merged.append(right[right_index])
+            right_index += 1
+
+    merged.extend(left[left_index:])
+    merged.extend(right[right_index:])
+
+    return merged
+
+
+def merge_sort_with_steps(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    left_half = merge_sort_with_steps(left_half)
+    right_half = merge_sort_with_steps(right_half)
+
+    print("Merging:", left_half, right_half)
+    merged_result = merge(left_half, right_half)
+    print("Merged:", merged_result)
+
+    return merged_result
+
+
+if __name__ == "__main__":
+    input_list = [3, 5, 8, 6, 7, 4, 1, 2, 9]
+    print("Original Array:", input_list, '\n')
+
+    sorted_array = merge_sort_with_steps(input_list)
+    print("\nFinal Sorted Array:", sorted_array)
